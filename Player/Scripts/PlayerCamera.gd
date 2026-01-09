afebd6816a960364eced92c4008bb7ac467de63b
Devesh <deveshsaket0@gmail.com>
2026-01-10 02:20:25 +0530

    10/1 Enemy

--- Player/Scripts/PlayerCamera.gd
+++ Player/Scripts/PlayerCamera.gd
@@ -3,10 +3,15 @@ class_name PlayerCamera extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	LevelManager.TileMapBoundsChanged.connect(UpdateLimits)
+	UpdateLimits(LevelManager.curret_titlemap_bounds)
 	pass # Replace with function body.
 
 
 func UpdateLimits(bounds: Array[Vector2]) -> void:
+	print(bounds)
+	if bounds == []:
+		return
 	limit_left = int(bounds[0].x)
 	limit_top = int(bounds[0].y)
 	limit_right = int(bounds[1].x)
